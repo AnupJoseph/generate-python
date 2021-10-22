8ad5202c78ba8edbe9e554cf0977a7e58ac4945c
AnupJoseph <anup20joseph@gmail.com>
2021-10-22 12:33:04 +0530

    Code for download done

---
+++ get_data.py
@@ -0,0 +1,41 @@
+from github import Github
+from rich import print
+
+import time
+from datetime import datetime
+import os
+
+end_time = time.time()
+start_time = end_time - 86400
+
+
+def check_query(library):
+    t_query = f"{library} language:python created:2021-10-01..2021-10-03"
+    result = g.search_repositories(t_query)
+    print(f"{library} = {result.totalCount}")
+    return result
+
+
+with open("token.txt") as file_pointer:
+    ACCESS_TOKEN = file_pointer.read()
+    g = Github(ACCESS_TOKEN)
+    # print(g.get_user())
+
+    for i in range(5):
+        start_time_str = datetime.utcfromtimestamp(start_time).strftime("%Y-%m-%d")
+        end_time_str = datetime.utcfromtimestamp(end_time).strftime("%Y-%m-%d")
+
+        query = f"altair language:python created:{start_time_str}..{end_time_str}"
+        print(query)
+        end_time -= 86400
+        start_time -= 86400
+
+        result = g.search_repositories(query)
+        print(result.totalCount)
+
+        for repository in set(result):
+            print(f"{repository.clone_url}")
+            os.system(
+                f"git clone {repository.clone_url} repos/{repository.owner.login}/{repository.name}"
+            )
+            break
