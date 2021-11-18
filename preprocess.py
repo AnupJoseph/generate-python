c120629f9006b5bac5e616ac69c46a4ec4916d94
AnupJoseph <anup20joseph@gmail.com>
2021-11-18 10:54:51 +0530

    Finished with parsing

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
-import os
-from tqdm import tqdm, notebook
+from rich import print
+from tqdm import tqdm,notebook
+
 from pathlib import Path
+import os
 
 MAX_CHAR_LENGTH = 512
 MIN_CHAR_LENGTH = 400
@@ -12,16 +14,22 @@ for dirpath, dirnames, filenames in tqdm(os.walk("repos")):
         full_path = dirpath / f
         full_paths.append(full_path)
 
-with open("python_code_text.txt","a") as file:
+with open("python_code_text.txt", "a") as file:
     for fpath in notebook.tqdm(full_paths):
-        with open(fpath, "r") as f:
-            d = f.read()
-            if 20 < len(d) <= MAX_CHAR_LENGTH:
+        try:
+            with open(fpath, "r") as f:
+                d = f.read()
                 fd = d.replace("\n", "<N>")
-                file.write(f"{fd} \n")
-                break
-            else:
-                sd = d.split("\n\n")
-                substring = ""
-                for split in sd:
-                    
+                if 20 < len(d) <= MAX_CHAR_LENGTH:
+                    file.write(f"{fd} \n")
+                    break
+                else:
+                    sd = fd.split("<N><N>")
+                    substring = ""
+                    for split in sd:
+                        substring += split + "<N><N>"
+                        if MIN_CHAR_LENGTH < len(substring) <= MAX_CHAR_LENGTH:
+                            f.write(substring + "\n")
+                            substring = ""
+        except Exception as e:
+            print(e)
